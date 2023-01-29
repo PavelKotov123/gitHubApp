c696e75bf1f647029a3ba6e200c836fbd2f3eeeb
Pavel <kotov29061995@gmail.com>
2023-01-29 21:34:35 +0300

    getResultStars

--- app/src/main/java/com/example/githubapp/MainActivity.kt
+++ app/src/main/java/com/example/githubapp/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.githubapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.githubapp.databinding.ActivityMainBinding
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //Запуск функции по нажатию
+        binding.bGet.setOnClickListener {
+            getResultStars()
+        }
     }
+    //Функция получения дынных по URL пользователя
+    private fun getResultStars(){
+        val url = "https://api.github.com/users/wt-t/starred"
+        val queue = Volley.newRequestQueue(this)
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            {
+                    response->
+//                val obj = JSONObject(response)
+//                val stars = obj.getJSONObject("full_name")    Попытка достать из response часть названия репозитория
+                Log.d("MyLog","Respone:$response")
+            },
+            {
+                Log.d("MyLog","Volley error:$it")
+            }
+        )
+        queue.add(stringRequest)
+    }
+
 }
